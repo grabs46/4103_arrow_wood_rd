0dcf7a90f9d9e03dce2e21ea15f4225789d5fd16
grabs46 <nicolasgrabner@gmail.com>
2024-05-28 12:34:31 -0500

    Basic Carousel working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+// routes/index.js
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require('express');
+const router = express.Router();
+
+// Sample array of image data
+const images = [
+    { url: '/images/FrontView.jpg', text: 'Text for Image 1' },
+    { url: '/images/BackYardWideView.jpg', text: 'Text for Image 2' },
+    { url: '/images/BackYardView2.jpg', text: 'Text for Image 3' }
+];
+
+router.get('/', (req, res) => {
+    res.render('index', { images });
 });
 
 module.exports = router;
